0a899a8c026921b7c009ecd901ba6e48f775185a
jose3215 <jose3215@gmail.com>
2017-08-09 14:49:03 -0600

    Login Activity, Google and Email finish it works well

---
+++ app/src/main/java/com/example/valkzer/motorizados/Activities/smsActivity.java
@@ -0,0 +1,15 @@
+package com.example.valkzer.motorizados.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.valkzer.motorizados.R;
+
+public class smsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+    }
+}
