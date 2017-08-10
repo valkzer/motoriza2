71674bbd7fd6c34fceae3a6b4128c87e9a3f1b37
valkzer <rvalcarcelvivas@gmail.com>
2017-08-10 14:12:03 -0600

    Changes: Add the detail delivery screen with buttons to call and sms.

--- app/src/main/java/com/example/valkzer/motorizados/Activities/DeliveryListActivity.java
+++ app/src/main/java/com/example/valkzer/motorizados/Activities/DeliveryListActivity.java
@@ -1,6 +1,9 @@
 package com.example.valkzer.motorizados.Activities;
 
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -11,6 +14,7 @@ import com.example.valkzer.motorizados.Models.Customer;
 import com.example.valkzer.motorizados.Models.Delivery;
 import com.example.valkzer.motorizados.Models.CreditCard;
 import com.example.valkzer.motorizados.Activities.Adapters.DeliveryListAdapter;
+import com.example.valkzer.motorizados.Activities.Listeners.RecyclerTouchListener;
 
 import java.util.List;
 import java.util.Observer;
@@ -21,15 +25,14 @@ public class DeliveryListActivity extends AppCompatActivity {
 
 
     private List<Delivery> deliveryList = new ArrayList<>();
-    private RecyclerView        recyclerView;
-    private DeliveryListAdapter deliveryListAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_list);
-
+        recyclerView = (RecyclerView) findViewById(R.id.deliveryList);
         createDummyRecord();
 
         Observer observer = new Observer() {
@@ -43,6 +46,25 @@ public class DeliveryListActivity extends AppCompatActivity {
 
         Delivery delivery = new Delivery();
         delivery.getAll(observer);
+        final Context context = this;
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position)
+            {
+                Delivery delivery = deliveryList.get(position);
+                Intent   intent   = new Intent(context, DeliveryDetailActivity.class);
+                intent.putExtra("deliveryId", delivery.getId());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongClick(View view, int position)
+            {
+
+            }
+        }));
+
+
     }
 
     private void createDummyRecord()
@@ -55,9 +77,8 @@ public class DeliveryListActivity extends AppCompatActivity {
 
     private void fillDeliveryList()
     {
-        recyclerView = (RecyclerView) findViewById(R.id.deliveryList);
-        deliveryListAdapter = new DeliveryListAdapter(deliveryList);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        DeliveryListAdapter        deliveryListAdapter = new DeliveryListAdapter(deliveryList);
+        RecyclerView.LayoutManager mLayoutManager      = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(deliveryListAdapter);
