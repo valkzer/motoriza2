9c1520c92e5e4ef40a65d39dc520e00dd9e72ec8
jose3215 <jose3215@gmail.com>
2017-08-18 14:14:54 -0600

    Layouts with some corrections

--- app/src/main/java/com/example/valkzer/motorizados/Activities/MainActivity.java
+++ app/src/main/java/com/example/valkzer/motorizados/Activities/MainActivity.java
@@ -48,15 +48,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -152,9 +144,7 @@ public class MainActivity extends AppCompatActivity
     {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        if (id == R.id.nav_Form) {
-            // Handle the camera action
-        } else if (id == R.id.nav_Delivery) {
+         if (id == R.id.nav_Delivery) {
             if(currentUser != null){
                 Intent loginIntent = new Intent(this, DeliveryListActivity.class);
                 startActivity(loginIntent);
